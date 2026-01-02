99ed0e4f2bb61358bf30771876f25aa9862f9b3e
noma <125211901+khawlanoman@users.noreply.github.com>
2026-01-02 15:49:37 +0000

    .

---
+++ ex0/ft_garden_intro.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+  print("=== Welcome to My Garden ===");
+  Plant = "Rose";
+  Height = 25;
+  age =  30;
+  print("Plant:",Plant);
+  print("Height:",Height, "cm");
+  print("Age:", age,"days");
+  print("=== End of Program ===");
---
+++ ex2/ft_plant_growth.py
@@ -0,0 +1,2 @@
+from ex1.ft_garden_data import Plant
+
